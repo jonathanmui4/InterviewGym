d55321a94ce419902b8179702a38a6c847e3b620
Jonathan Mui <jonathanmui4@gmail.com>
2025-03-28 18:25:46 +0800

    Add auth check for route management

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -19,13 +19,16 @@ export async function signUp(params: SignUpParams) {
             };
         }
 
-        await db.collection("users").doc(uid).set({
-            name: name,
-            email: email,
-            photoURL: photoUrl || "",
-            provider: providerId || "unknown",
-            createdAt: new Date(),
-        });
+        await db
+            .collection("users")
+            .doc(uid)
+            .set({
+                name: name,
+                email: email,
+                photoURL: photoUrl || "",
+                provider: providerId || "unknown",
+                createdAt: new Date(),
+            });
 
         return {
             success: true,
@@ -56,17 +59,23 @@ export async function signIn(params: SignInParams) {
 
         if (isSocial) {
             // Trying to fetch user by entering collection, getting document of uid
-            const userRecordDb = await db.collection("users").doc(userRecord.uid).get();
+            const userRecordDb = await db
+                .collection("users")
+                .doc(userRecord.uid)
+                .get();
 
             if (!userRecordDb.exists) {
-                await db.collection("users").doc(userRecord.uid).set({
-                    name: name,
-                    email: email,
-                    photoUrl: photoUrl || "",
-                    provider: providerId || "unknown",
-                    createdAt: new Date(),
-                });
-            }  
+                await db
+                    .collection("users")
+                    .doc(userRecord.uid)
+                    .set({
+                        name: name,
+                        email: email,
+                        photoUrl: photoUrl || "",
+                        provider: providerId || "unknown",
+                        createdAt: new Date(),
+                    });
+            }
         }
 
         if (!userRecord) {
@@ -109,4 +118,47 @@ export async function setSessionCookie(idToken: string) {
         path: "/", // Cookie available across entire site
         sameSite: "lax", // Provides some CSRF protection while allowing the cookie to be sent when navigating to the site from external links
     });
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+    // Retrieve application's cookie management interface
+    const cookieStore = await cookies();
+
+    // Get specific session cookie
+    const sessionCookie = cookieStore.get("session")?.value;
+
+    // Return null if unable to get session cookie
+    if (!sessionCookie) return null;
+
+    // If cookie does exist
+    try {
+        // Decode session to see if there is valid user
+        const decodedClaims = await auth.verifySessionCookie(
+            sessionCookie,
+            true
+        );
+
+        const userRecord = await db
+            .collection("users")
+            .doc(decodedClaims.uid)
+            .get();
+        if (!userRecord.exists) return null;
+
+        return {
+            ...userRecord.data(),
+            id: userRecord.id,
+        } as User;
+    } catch (e: any) {
+        console.log("Error retrieving current user: ", e);
+
+        // Invalid or expired session
+        return null;
+    }
+}
+
+export async function isAuthenticated() {
+    const user = await getCurrentUser();
+
+    // If obj contains user data, return true, else false
+    return !!user;
 }
