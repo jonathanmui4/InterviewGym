cb1b9e75fbe461cc8817d7a1c0cbe41881ce8c0e
Jonathan Mui <jonathanmui4@gmail.com>
2025-04-05 20:10:29 +0800

    Fix bug to display profile pic in call

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,20 @@ const nextConfig: NextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                pathname: "/a/**",
+            },
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
